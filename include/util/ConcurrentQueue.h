df61d3ed45b9c5b47dab7b2464f6c44d97e20f8a
Mbazooka <122548181+Mbazooka@users.noreply.github.com>
2026-02-04 12:04:09 +0800

    fixed building issue due to templates

--- include/util/ConcurrentQueue.h
+++ include/util/ConcurrentQueue.h
@@ -13,10 +13,27 @@ class ConcurrentQueue {
 
 public:
     ConcurrentQueue() = default;
-    void enqueue(T value);
-    T dequeue();
-    // Determines whether the queue is non-empty. Bad interleavings may occur
-    bool is_queued();
+    void enqueue(T value) {
+        std::lock_guard lock(m);
+        q.push(value);
+    }
+
+    T dequeue() {
+        std::lock_guard lock(m);
+        if (!q.empty()) {
+            T value = q.front();
+            q.pop();
+            return value;
+        } else {
+            throw std::runtime_error("Queue is empty");
+        }
+    }
+
+    // Determines whether the queue is non-empty. Bad interleavings may occur.
+    bool is_queued() {
+        std::lock_guard lock(m);
+        return !q.empty();
+    }
 
 private:
     std::queue<T> q;
