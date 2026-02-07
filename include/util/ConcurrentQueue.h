71ac21b62a1e9d9298385be2d424a3cdd2d279bd
Mbazooka <maboum2000@gmail.com>
2026-02-07 19:03:13 +0800

    Adjusting representation of dequeue using optional values

--- include/util/ConcurrentQueue.h
+++ include/util/ConcurrentQueue.h
@@ -1,12 +1,10 @@
-//
-// Created by Mathis Boumaza on 03/02/2026.
-//
-
 #ifndef CACTOR_CONCURRENTQUEUE_H
 #define CACTOR_CONCURRENTQUEUE_H
 #include <queue>
 #include <vector>
 
+// TO DO:
+// Make it lock free
 
 template <typename T>
 class ConcurrentQueue {
@@ -18,28 +16,21 @@ public:
         q.push(value);
     }
 
-    T dequeue() {
+    ~ConcurrentQueue() = default;
+
+    std::optional<T> dequeue() {
         std::lock_guard lock(m);
         if (!q.empty()) {
             T value = q.front();
             q.pop();
             return value;
-        } else {
-            throw std::runtime_error("Queue is empty");
         }
-    }
-
-    // Determines whether the queue is non-empty. Bad interleavings may occur.
-    bool is_queued() {
-        std::lock_guard lock(m);
-        return !q.empty();
+        return std::nullopt;
     }
 
 private:
     std::queue<T> q;
     std::recursive_mutex m;
-
-
 };
 
 
